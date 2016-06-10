cc995a27bed5549e94965ee41466ddebb503668e
timmmooney <mooney@aps.anl.gov>
2016-06-10 14:39:08 -0500

    new file:   iocBoot/iocSoftGlueZynq/SGMenu.req new file:   iocBoot/iocSoftGlueZynq/loadFPGA.sh new file:   iocBoot/iocSoftGlueZynq/loadFPGA_3_IO.sh new file:   iocBoot/iocSoftGlueZynq/softGlue.iocsh

---
+++ iocBoot/iocSoftGlueZynq/loadFPGA.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cat /xorApps/zed/design_1_wrapper.bin >/dev/xdevcfg
+#cat /xorApps/zed/design_1_wrapper_3f.bin >/dev/xdevcfg
---
+++ iocBoot/iocSoftGlueZynq/loadFPGA_3_IO.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /xorApps/zed/design_1_wrapper_3_IO.bin >/dev/xdevcfg
